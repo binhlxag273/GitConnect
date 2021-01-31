1de04d8621c3dac271481484ad44e3a398e77b23
binhlxag273 <binhlxag273@gmail.com>
2021-01-31 23:10:15 +0700

    Commit

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal*years*rate/100;
-var futureYear = new Date().getFullYear() + years;
 
 function compute()
 {
-  p = document.getElementById("principal").value;
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = parseInt(document.getElementById("years").value);
+  var interest = principal*years*rate/100;
+  var futureYear = parseInt(new Date().getFullYear())  + years;
+  document.getElementById("result").innerText = "If your deposit " + principal
+  + ",\nat an interest rate of " + rate +".\nYou will receive an amount of "
+  + (parseFloat(principal) + parseFloat(interest)) + ",\nin the year " + futureYear;
 }
 
 function rateShow()
 {
-  document.getElementById("displayRate").innerText = rate;
+  var rate = document.getElementById("rate").value;
+  document.getElementById("displayRate").innerText = rate+"";
 }
+
+var principal = document.getElementById("principal");
+principal.addEventListener('keyup', (e) => {
+  if(principal.value==="")
+    document.getElementById("ComputeInterest").disabled = true;
+  else
+  {
+    document.getElementById("ComputeInterest").disabled = false;
+    if(e.keyCode==13) compute(); //xong
+  }
+});
