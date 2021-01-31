f40d73ea0df4fa62403a6a7dfbfa5fa543b695bc
binhlxag273 <binhlxag273@gmail.com>
2021-01-31 21:45:38 +0700

    Commit

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal*years*rate/100;
+var futureYear = new Date().getFullYear() + years;
+
 function compute()
 {
-  var principal =
   p = document.getElementById("principal").value;
+}
+
+function rateShow()
+{
+  document.getElementById("displayRate").innerText = rate;
 }
